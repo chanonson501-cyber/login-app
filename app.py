d45982a91d9befa84261ab8bf8f78f0d58d635e7
Chanon S. <chanonson51@gmail.com>
2026-09-24 19:16:08 +0700

    update frontend with modal login

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ def home():
     # เปลี่ยนให้เรียกใช้งานไฟล์หลักของคุณ (เช่น main.html)
     return render_template('main.html')
 
+@app.route('/reference')
+def reference():
+    return render_template('reference.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
